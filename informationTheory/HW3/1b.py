6750a74a372cc43e80eb1f37738f907f8015f92a
brianhsu7476 <brianhsu7476@gmail.com>
2023-10-18 20:01:36 +0800

    finish p3

--- informationTheory/HW3/1b.py
+++ informationTheory/HW3/1b.py
@@ -7,17 +7,17 @@ def addPoint(x, y, label):
 
 p0, p1=0.2, 0.3
 pt=[]
-for i in [j/10 for j in range(11)]:
-	pt.append([p0+i*(1-p0), (1-i)*(1-p1)])
-	pt.append([i*p0, (1-p1)+(1-i)*p1])
+for i in range(10000):
+	pt.append([1-(1-p0)**i, (1-p1)**i])
 pt.sort()
-plt.plot([i[0] for i in pt], [i[1] for i in pt])
-plt.scatter([0, p0, 1], [1, 1-p1, 0], marker='o')
+plt.plot([i[0] for i in pt], [i[1] for i in pt], marker='o')
 addPoint(0, 1, 'none')
 addPoint(p0, 1-p1, '$(p_0, 1-p_1)$')
+addPoint(1-(1-p0)**2, (1-p1)**2, '$(1-(1-p_0)^2, (1-p_1)^2)$')
+addPoint(1-(1-p0)**3, (1-p1)**3, '$(1-(1-p_0)^3, (1-p_1)^3)$')
 addPoint(1, 0, 'none')
 plt.xlabel('$\pi_{1|0}$')
 plt.ylabel('$\pi_{0|1}$')
 plt.legend()
-plt.savefig('1a.png')
+plt.savefig('1b.png')
 plt.show()
